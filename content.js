b9787bc7401bf362a4c3cfd6625ee482d04482dc
Justin Antony <justinantony92@gmail.com>
2017-09-14 22:18:00 -0400

    Added some werds

--- content.js
+++ content.js
@@ -16,6 +16,17 @@ for (var i = 0; i < elements.length; i++) {
             var replacedText = text.replace(/(Casino)/gi, 'Crasino');
             var replacedText = text.replace(/(Orangutan)/gi, 'Dangatang');
             var replacedText = text.replace(/(Kayak)/gi, 'Krayak');
+            var replacedText = text.replace(/(Davis)/gi, 'Dungus');
+            var replacedText = text.replace(/(Penis)/gi, 'Prennis');
+            var replacedText = text.replace(/(Prisoners)/gi, 'Brad Boy Prunks');
+            var replacedText = text.replace(/(Altar Boy)/gi, 'Church Boy');
+            var replacedText = text.replace(/(God)/gi, 'Gord');
+            var replacedText = text.replace(/(Jesus)/gi, 'Jingus');
+            var replacedText = text.replace(/(Zoo)/gi, 'Zew');
+            var replacedText = text.replace(/(Mercury)/gi, 'Mercerus');
+            var replacedText = text.replace(/(Pluto)/gi, 'Plunto');
+            var replacedText = text.replace(/(Earth)/gi, 'Water Planet');
+            var replacedText = text.replace(/(Neptune)/gi, 'Doris');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
